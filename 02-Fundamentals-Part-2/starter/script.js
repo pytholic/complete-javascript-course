7e60d49315a47be80b229b0069dfa65f75cd9e79
pytholic <rajahaseeb147@gmail.com>
2023-02-08 08:43:11 +0900

    Developer skills and editor setup

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -358,15 +358,15 @@
 //   totals.push(tip + bills[i]);
 // }
 
-// console.log(bills, tips, totals);
+console.log(bills, tips, totals);
 
-// const calcAverage = function (arr) {
-//   let sum = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     sum += arr[i];
-//   }
-//   return sum / arr.length;
-// };
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
 
-// console.log(calcAverage(totals));
-// console.log(calcAverage(tips));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,51 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// Temperature amplitude is the differenc ebetween hgihest and lowest temperature
+// How to compute max and min temperature
+// What to do about sensor error?
+
+// Ignore temp
+// Find min and max
+// Subtract min from amx and return it
+
+// function calcTempAmplitude(temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const currTemp = temps[i];
+//     if (typeof currTemp != "number") continue;
+
+//     if (currTemp > max) max = currTemp;
+//     if (currTemp < min) min = currTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// }
+
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+function calcTempAmplitudeNew(t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const currTemp = temps[i];
+    if (typeof currTemp != "number") continue;
+
+    if (currTemp > max) max = currTemp;
+    if (currTemp < min) min = currTemp;
+  }
+  console.log(max, min);
+  return max - min;
+}
+
+const amplitude = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitude);
